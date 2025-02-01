2d6d3476acd6413baf7fdaed487323127df375a3
EnotNK1 <wowova@bk.ru>
2025-02-01 16:13:07 +0700

    add celery and images router

--- src/main.py
+++ src/main.py
@@ -6,7 +6,8 @@ from src.api.hotels import router as router_hotels
 from src.api.rooms import router as router_rooms
 from src.api.auth import router as router_auth
 from src.api.bookings import router as router_bookings
-from src.api.facilities import router as router_facility
+from src.api.facilities import router as router_facilities
+from src.api.images import router as router_images
 import sys
 from pathlib import Path
 from fastapi_cache import FastAPICache
@@ -29,7 +30,8 @@ app.include_router(router_auth)
 app.include_router(router_hotels)
 app.include_router(router_rooms)
 app.include_router(router_bookings)
-app.include_router(router_facility)
+app.include_router(router_facilities)
+app.include_router(router_images)
 
 @app.get("/docs", include_in_schema=False)
 async def custom_swagger_ui_html():
