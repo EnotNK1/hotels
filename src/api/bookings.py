679b3a0d2e10c445cfd8cb996493bca20d621026
EnotNK1 <wowova@bk.ru>
2025-02-05 07:04:22 +0700

    create add_booking()

--- src/api/bookings.py
+++ src/api/bookings.py
@@ -14,8 +14,9 @@ async def create_booking(
         booking_data: BookingAddRequest
 ):
     room = await db.rooms.get_one_or_none(id=booking_data.room_id)
+    hotel = await db.hotels.get_one_or_none(id=room.hotel_id)
     _booking_data = BookingAdd(price=room.price, user_id=user_id, **booking_data.model_dump())
-    booking = await db.bookings.add(_booking_data)
+    booking = await db.bookings.add_booking(_booking_data, hotel_id=hotel.id)
     await db.commit()
 
     return {"status": "OK", "data": booking}
--- src/repositories/bookings.py
+++ src/repositories/bookings.py
@@ -4,6 +4,9 @@ from datetime import date
 from src.models.bookings import BookingsOrm
 from src.repositories.base import BaseRepository
 from src.repositories.mappers.mappers import BookingDataMapper
+from src.repositories.utils import rooms_ids_for_booking
+from src.schemas.bookings import BookingAdd
+
 
 class BookingsRepository(BaseRepository):
     model = BookingsOrm
@@ -13,3 +16,17 @@ class BookingsRepository(BaseRepository):
         query = select(BookingsOrm).filter(BookingsOrm.date_to == date.today())
         res = await self.session.execute(query)
         return [self.mapper.map_to_domain_entity(booking) for booking in res.scalars().all]
+
+    async def add_booking(self, data: BookingAdd, hotel_id: int):
+        rooms_ids_to_get = rooms_ids_for_booking(
+            date_from=data.date_from,
+            date_to=data.date_to,
+            hotel_id=hotel_id,
+        )
+        rooms_ids_to_book_res = await self.session.execute(rooms_ids_to_get)
+        rooms_ids_to_book: list[int] = rooms_ids_to_book_res.scalars().all()
+        if data.room_id in rooms_ids_to_book:
+            new_booking = await self.add(data)
+            return new_booking
+        else:
+            raise Exception
